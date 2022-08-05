816a9aef4f944362077af653b90dab706ba37068
Alan <alan.andersonbr@gmail.com>
2022-08-05 13:20:09 -0300

    teste banco 03

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -19,27 +19,36 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
             //Criar tabela
-            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT(3) )");
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INT(3) )");
 
             //Inserir dados
-            //bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Priscilla', 27)");
-            //bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Lydice', 21) ");
-            //bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Norman Powell', 29) ");
+            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Priscilla', 27)");
+            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Lydice', 21) ");
+            bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Norman Powell', 29) ");
+            //atualizando dado de uma pessoa tabela
+            //bancoDados.execSQL("UPDATE pessoas SET idade = 19 WHERE nome = 'Pavon'");
+
+            //apagar tabela
+            //bancoDados.execSQL("DROP TABLE pessoas");
+
             //recuperar pessoas
             String consulta =
-                    "SELECT nome, idade" +
-                    " FROM pessoas WHERE 1=1 ORDER BY idade ASC ";
+                    "SELECT id, nome, idade" +
+                    " FROM pessoas" +
+                            " WHERE 1=1";
             Cursor cursor = bancoDados.rawQuery(consulta, null);
 
             //inndices tabela
+            int indiceId = cursor.getColumnIndex("id");
             int indiceNome = cursor.getColumnIndex("nome");
             int indiceIdade = cursor.getColumnIndex("idade");
             cursor.moveToFirst();
             while (cursor != null) {
                 String nome = cursor.getString(indiceNome);
                 String idade = cursor.getString(indiceIdade);
+                String id = cursor.getString(indiceId);
 
-                Log.i("RESULTADO - nome ", nome + " / idade: " + idade);
+                Log.i("RESULTADO - id ", id + "/ nome: " + nome + "/ idade " + idade );
 
                 cursor.moveToNext();
 
